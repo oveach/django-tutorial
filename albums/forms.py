51ebadff48b19c4a001df27e7a50f3bec4ed2e10
ovh <oveach@gmail.com>
2014-05-08 02:02:59 +0200

    Improve album form presentation

--- albums/forms.py
+++ albums/forms.py
@@ -3,5 +3,5 @@
 from django import forms
 
 class AlbumForm(forms.Form):
-    artist = forms.CharField(required=True, max_length=100, label="Artist")
-    title = forms.CharField(required=True, max_length=100, label="Title")
+    artist = forms.CharField(required=True, max_length=100, label="Artist", widget=forms.TextInput(attrs={'size': 80}))
+    title = forms.CharField(required=True, max_length=100, label="Title", widget=forms.TextInput(attrs={'size': 80}))
